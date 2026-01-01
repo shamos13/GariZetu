6ae6840c5843f903068b1410d771c7769911a9a2
shamos13 <akwatuha04@gmail.com>
2026-01-01 16:38:22 +0300

    Modified homepage to render pages

---
+++ FrontEnd/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import {HeroSection} from "../components/HeroSection.tsx";
+import {Navbar} from "../components/Navbar.tsx";
+
+export default function HomePage() {
+    return (
+        <>
+            <Navbar/>
+            <HeroSection/>
+        </>
+    )
+}
