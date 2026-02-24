1ac64a4151529770c4603f8276860b515467f60f
shamos13 <akwatuha04@gmail.com>
2026-02-24 17:47:17 +0300

    Optimizations

--- Backend/GariZetu/src/main/java/com/amos/garizetu/Repository/FeatureRepository.java
+++ Backend/GariZetu/src/main/java/com/amos/garizetu/Repository/FeatureRepository.java
@@ -3,12 +3,15 @@ package com.amos.garizetu.Repository;
 import com.amos.garizetu.Car.Entity.Feature;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 public interface FeatureRepository extends JpaRepository<Feature, Long> {
 
     Optional<Feature> findByFeatureName(String name);
+
+    List<Feature> findByFeatureNameIn(Set<String> names);
 
     Set<Feature> findByFeatureCategory(String category);
 
